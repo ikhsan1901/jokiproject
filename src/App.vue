00af826a5a7417a945a6cacadd8d476ccfb98337
ikhsan1901 <120877987+ikhsan1901@users.noreply.github.com>
2023-05-15 21:39:37 +0700

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -76,7 +76,7 @@ export default {
       const s1 = document.createElement('script');
       const s0 = document.getElementsByTagName('script')[0];
       s1.async = true;
-      s1.src = 'https://embed.tawk.to/YOUR_TAWKTO_WIDGET_CODE';
+      s1.src = 'https://embed.tawk.to/6462427874285f0ec46b8f4b/1h0ft79u7';
       s1.charset = 'UTF-8';
       s1.setAttribute('crossorigin', '*');
       s0.parentNode.insertBefore(s1, s0);
