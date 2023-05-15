2bd771afb8eb3c0d052a21dc3715b900c5c18c19
ikhsan1901 <120877987+ikhsan1901@users.noreply.github.com>
2023-05-15 21:09:31 +0700

    Update Home.vue

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -105,7 +105,7 @@ export default {
   name: 'Home',
   data() {
     return {
-      heroImage: '../src/assets/2.png',
+      heroImage: 'https://i.postimg.cc/fbMj9VPX/logos.png',
       heroTitle: 'Joki Tugasku',
       heroDescription: 'Lebih mudah dan cepat bersama Joki Tugasku!',
       heroButton: 'Pesan Sekarang'
