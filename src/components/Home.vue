9a4434f3de9e7f87138b089d2895430296259472
ikhsan1901 <120877987+ikhsan1901@users.noreply.github.com>
2023-05-15 21:34:06 +0700

    Update Home.vue

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,4 +1,17 @@
 <template>
+<!--Start of Tawk.to Script-->
+<script type="text/javascript">
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/6462427874285f0ec46b8f4b/1h0ft79u7';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+</script>
+<!--End of Tawk.to Script-->
   <div class="home">
     <div class="hero-section">
       <div class="hero-content">
